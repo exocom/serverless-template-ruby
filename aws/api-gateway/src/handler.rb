26ec5f0ccc7d64e36218c73d6806b49e2cb96993
Kalarrs Topham <k-dawg-klakr@hotmail.com>
2018-12-01 16:29:39 -0700

    working ruby code

--- aws/api-gateway/src/handler.rb
+++ aws/api-gateway/src/handler.rb
@@ -1,14 +1,14 @@
 #!./ruby/bin/ruby
 require 'json'
-require 'bson'
+# require 'bson'
 
-headers = {
+$headers = {
     :'Access-Control-Allow-Origin' => '*',
     :'Access-Control-Allow-Credentials' => true,
     :'Content-Type' => 'application/json'
 }
 
-changes = [
+$changes = [
     {
         id: '5a1b5ae36758c40453e5e024',
         description: 'This is an example'
@@ -19,177 +19,10 @@ changes = [
     }
 ]
 
-get_changes = lambda do |event|
-  puts JSON.generate(
+def get_changes(event:, context:)
+  {
       statusCode: 200,
-      body: {data: changes},
-      headers: headers
-  )
+      body: JSON.generate({data: $changes}),
+      headers: $headers
+  }
 end
-
-post_changes = lambda do |event|
-  begin
-    event = JSON.parse(event)
-
-    begin
-      unless event.key?('body')
-        raise "missing id"
-      end
-
-      body = event['body']
-
-      unless body.key?('description')
-        raise "missing description"
-      end
-      unless body.key?('id')
-        raise "missing id"
-      end
-
-      id = BSON::ObjectId(body['id'])
-    rescue Exception => e
-      puts JSON.generate(
-          statusCode: 400,
-          body: {
-              error: {
-                  type: 'ApiError',
-                  message: 'Invalid change provided. Please provide id and description.' + e.message
-              }
-          },
-          headers: headers
-      )
-      return
-    end
-
-    if changes.find {|c| c[:id] == body['id']}
-      puts JSON.generate(
-          statusCode: 409,
-          body: {
-              error: {
-                  type: 'ApiError',
-                  message: 'A change with that Id already exits.'
-              }
-          },
-          headers: headers
-      )
-      return
-    end
-
-    change = {
-        id: body['id'],
-        description: body['description']
-    }
-
-    changes.push(change)
-
-    puts JSON.generate(
-        statusCode: 201,
-        body: {data: change},
-        headers: headers
-    )
-
-  rescue Exception => e
-    puts JSON.generate(
-        statusCode: 500,
-        body: {
-            error: {
-                type: 'ApiError',
-                message: e&.message
-            }
-        },
-        headers: headers
-    )
-  end
-end
-
-get_change = lambda do |event|
-  begin
-    event = JSON.parse(event)
-    unless event.key?('pathParameters') || event['pathParameters'].key('changeId')
-      raise "missing changeId"
-    end
-
-  rescue Exception => e
-    puts JSON.generate(
-        statusCode: 400,
-        body: {
-            error: {
-                type: 'ApiError',
-                message: 'Invalid change provided. Please provide id and description.' + e.message
-            }
-        },
-        headers: headers
-    )
-    return
-  end
-
-  change_id = event['pathParameters']['changeId']
-  change = changes.detect {|c| c[:id] == change_id}
-  unless change
-    puts JSON.generate(
-        statusCode: 404,
-        body: {
-            error: {
-                type: 'ApiError',
-                message: 'No change was found with the given id.'
-            }
-        },
-        headers: headers
-    )
-    return
-  end
-
-  puts JSON.generate(
-      statusCode: 200,
-      body: {data: change},
-      headers: headers
-  )
-end
-
-delete_change = lambda do |event|
-  begin
-    event = JSON.parse(event)
-    unless event.key?('pathParameters') || event['pathParameters'].key('changeId')
-      raise "missing changeId"
-    end
-
-  rescue Exception => e
-    puts JSON.generate(
-        statusCode: 400,
-        body: {
-            error: {
-                type: 'ApiError',
-                message: 'Invalid change provided. Please provide id and description.' + e.message
-            }
-        },
-        headers: headers
-    )
-    return
-  end
-
-  change_id = event['pathParameters']['changeId']
-  change = changes.detect {|c| c[:id] == change_id}
-  unless change
-    puts JSON.generate(
-        statusCode: 404,
-        body: {
-            error: {
-                type: 'ApiError',
-                message: 'No change was found with the given id.'
-            }
-        },
-        headers: headers
-    )
-    return
-  end
-
-  changes.delete(change)
-
-  puts JSON.generate(
-      statusCode: 204,
-      headers: headers
-  )
-end
-
-handler = eval(ARGV[0])
-# TODO : check if handler null and then return error?
-handler.call(ARGV[1])
